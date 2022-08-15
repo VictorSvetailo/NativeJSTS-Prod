a8ecfe56d9f4fe44287c3b340345a1e3634dcc73
VictorSvetailo <victorioeve@gmail.com>
2022-08-15 12:39:44 +0300

    Lesson #29 method

--- script.js
+++ script.js
@@ -202,42 +202,7 @@ confirm(result)
 
 
 //
-// 'use strict';
-//
-// let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-//
-//
-//
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
-//
-// for (let i = 0; i < 2; i++) {
-//     const a = prompt('Один из последних просмотренных фильмов?', ''),
-//         b = prompt('На сколько оцените его?', '');
-//
-//         if (a != null && b != null && a !== '' && b !== "" && a.length < 50){
-//             personalMovieDB.movies[a] = b;
-//             console.log('Done')
-//         }else{
-//             console.log('Error')
-//             i--;
-//         }
-//
-// }
-//
-// personalMovieDB.count === 0 || '' ? alert("Произошла ошибка") :
-//     personalMovieDB.count <= 10 ? alert("Просмотрено довольно мало фильмов") :
-//         personalMovieDB.count > 10 && personalMovieDB.count <= 30 ? alert("Вы классический зритель") :
-//             personalMovieDB.count > 30 ? alert("Вы киноман") : alert("Произошла ошибка")
-//
-//
-// console.log(personalMovieDB);
-//
+
 
 
 //
