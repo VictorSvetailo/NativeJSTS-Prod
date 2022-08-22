213115c25bf8d82a101cb2486b52457c3baf27c4
VictorSvetailo <victorioeve@gmail.com>
2022-08-22 11:43:30 +0300

    Lesson #42 document

--- script30-40.js
+++ script30-40.js
@@ -1,29 +1,42 @@
 'use state'
 
 
-function createCounter(){
-
-    let counter = 0;
-
-    const myFunction = function(){
-      counter = counter + 1
-        return counter;
-
-    }
-    return myFunction;
-
-}
-
-const increment = createCounter();
-console.log(increment + ' hello')
-const c1 = increment();
-const c2 = increment();
-const c3 = increment();
-const c5 = increment();
-
-console.log(c1, c2, c3, c5)
 
 
+
+//
+// const arraySort = ['d', 'f', 'a', 'c', 'b']
+//
+//
+//
+//
+// console.log(arraySort.sort().reverse())
+//
+
+
+// function createCounter(){
+//
+//     let counter = 0;
+//
+//     const myFunction = function(){
+//       counter = counter + 1
+//         return counter;
+//
+//     }
+//     return myFunction;
+//
+// }
+//
+// const increment = createCounter();
+// console.log(increment + ' hello')
+// const c1 = increment();
+// const c2 = increment();
+// const c3 = increment();
+// const c5 = increment();
+//
+// console.log(c1, c2, c3, c5)
+//
+//
 
 
 
