deb4ed41ed74cbf5ad6eb3c1c0489ae9e9343386
VictorSvetailo <victorioeve@gmail.com>
2022-12-14 20:29:39 +0300

    adjustment(DOM)

---
+++ java-script/element/elements 2/js/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// const box = document.getElementById('box');
+// console.log(box)
+// получаем псевдо массив  у него нет методов как у обычного массива  // HTML коллекция
+// const btn = document.getElementsByTagName("button");
+// console.log(btn)
+// получить один элемент
+// const btn = document.getElementsByTagName("button");
+// console.log(btn[1])
+
+// находим элементы по классу это тоже HTML коллекция
+// const circles = document.getElementsByClassName('circle')
+// console.log(circles)
+
+// этот метод устроен так что внутрь мы помещаем css силектор
+// querySelectorAll есть метод forEach()
+// const hearts = document.querySelectorAll('.heart')
+// hearts.forEach((item)=>{
+//     console.log(item)
+// })
+// console.log(hearts)
+
+// достает одн элемент и именно первый элемент со страницы
+// const oneHeart = document.querySelector('div')
+// console.log(oneHeart)
+
