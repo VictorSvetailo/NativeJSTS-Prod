e7881bc8af34ca5468489fe16a29cac0959d3e58
VictorSvetailo <victorioeve@gmail.com>
2023-01-12 16:38:45 +0300

    feat() requests to the local server. axios

---
+++ java-script/array/array-iteration-methods/array-iteration.js
@@ -0,0 +1,39 @@
+'use strict'
+
+// const names = ['Ivan', 'Anna', 'Gena', 'Victor', 'Afanasiy']
+//
+// const findName = names.find(el => el === 'Victor')
+// console.log(findName)
+//
+//
+// const newArray = names.filter(el => el.length > 5)
+//
+// console.log(newArray)
+//
+//
+// const nameChange = ['IvAn', 'ANNA', 'SerGei', 'GoSha'].map(el => el.toLowerCase())
+//
+// // const newNameChange = nameChange.map(el => el.toLowerCase())
+// console.log(nameChange)
+
+// every/some
+// const some = ['Eba', 'Hello', 'Test']
+// console.log(some.every(el => typeof(el) === 'string'))
+
+
+// const arr = ['Ivan', 'Anna', 'Gena', 'Victor']
+// const reduce = arr.reduce((sum, current) => `${sum}, ${current}`)
+//
+// console.log(reduce)
+
+
+// const obj = {
+//     vic: 'person',
+//     maria: 'person',
+//     dog: 'animal',
+//     cat: 'animal',
+// }
+// const newArray = Object.entries(obj)
+//     .filter(el => el[1] === 'person')
+//     .map(el => el[0])
+// console.log(newArray)
